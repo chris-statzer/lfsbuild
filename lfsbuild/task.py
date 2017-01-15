b8bc6cadc3416b6861524c3c467ebddb5d80c980
Chris Statzer <chris.statzer@gmail.com>
2017-01-16 01:52:53 +0800

    First package building

--- lfsbuild/task.py
+++ lfsbuild/task.py
@@ -3,11 +3,11 @@ import logging
 import os
 import glob
 
-
 import download
 import config
 
 log = logging.getLogger('TASK')
+
 
 class Task(object):
     """docstring for Task."""
@@ -25,22 +25,44 @@ class Task(object):
         md5 = self.data['md5']
         filename = url[url.rfind('/')+1:]
 
-        if download.download(url, md5) is True:
-            tar_flags = ''
-            if url[-2:] == 'gz':
-                tar_flags = 'xzf'
-            elif url[-2:] == 'xz':
-                tar_flags = 'xJf'
-            extract_cmd = 'tar {} {}/{} -C {}'.format(tar_flags, config.CACHE_PATH, filename, config.SOURCE_PATH)
-            log.info('Extracting {} to {}'.format(filename, config.SOURCE_PATH))
-            return_code = os.system(extract_cmd)
+        # Download the package if we dont already have it
+        if download.download(url, md5) is False:
+            return False
+
+        # extract the package to the src directory
+        tar_flags = ''
+        if url[-2:] == 'gz':
+            tar_flags = 'xzf'
+        elif url[-2:] == 'xz':
+            tar_flags = 'xJf'
+        extract_cmd = 'tar {} {}/{} -C {}'.format(tar_flags, config.CACHE_PATH, filename, config.SOURCE_PATH)
+        log.info('Extracting {} to {}'.format(filename, config.SOURCE_PATH))
+        return_code = os.system(extract_cmd)
+        if return_code != 0:
+            log.error('Error extracting {}'.format(filename))
+            return False
+
+        # delete old build directory if it exists
+        if os.path.isdir(self.build_path):
+            os.system('rm -rf {}'.format(self.build_path))
+
+        # now create the build path and change to it
+        os.system('mkdir {}'.format(self.build_path))
+        os.chdir(self.build_path)
+
+        os.system('../../{}/configure'.format(self.src_path))
+        os.system('make')
     @property
     def name(self):
         return self.data['name']
 
     @property
-    def type(self):
-        return self.data['type']
+    def src_path(self):
+        return '{}/{}'.format(config.SOURCE_PATH, self.data['dir_name'])
+
+    @property
+    def build_path(self):
+        return '{}/{}'.format(config.BUILD_PATH, self.name)
 
 
 def load_tasks(path=config.TASK_PATH):
